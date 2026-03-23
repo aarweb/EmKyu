e481d99d04464792ac05b7cd0c19f6447ac81673
AarMagic <aaroof04@gmail.com>
2026-03-23 18:59:33 +0100

    feat: Implementar BinanceBroker y mapeo de datos para Binance

--- scrapping/src/broker/binance.py
+++ scrapping/src/broker/binance.py
@@ -1,1 +1,53 @@
-# TO-DO -> Aarón, el criptomaster
+import json
+from typing import override
+
+import websockets
+from websockets.asyncio.client import connect
+
+from broker.mapper.binance import BinanceDataMapper
+from broker.mapper.model.time_series_cripto import TSData
+from env.binance import BINANCE_WS
+
+from .client import BrokerClient
+
+
+class BinanceBroker(BrokerClient):
+    @staticmethod
+    def create() -> BrokerClient:
+        return BinanceBroker(
+            BINANCE_WS,
+            "BINANCE",
+            args={
+                "method": "SUBSCRIBE",
+                "params": [
+                    "btcusdt@trade",
+                    "dogeusdt@trade",
+                    "ethusdt@trade",
+                    "solusdt@trade",
+                ],
+                "id": 1,
+            },
+        )
+
+    @override
+    async def connect(self):
+        self.client = await connect(self.url)
+        _ = await self.client.send(json.dumps(self.args))
+        _ = await self.client.recv()
+
+    @override
+    async def onListen(self):
+        try:
+            data = json.loads(await self.client.recv())
+            mapped: TSData = BinanceDataMapper.mapResponse(data)
+            print(mapped)
+        except websockets.exceptions.ConnectionClosed:
+            await self.connect()
+        except Exception as e:
+            await self.close()
+            print(f"Error processing Binance data: {e}")
+
+    @override
+    async def close(self):
+        if self.client:
+            await self.client.close()
--- scrapping/src/main.py
+++ scrapping/src/main.py
@@ -2,11 +2,12 @@ import asyncio
 
 from broker.bybit import BybitBroker
 from broker.client import BrokerClient
+from broker.binance import BinanceBroker
 
 
 async def main():
 
-    brokers: list[BrokerClient] = [BybitBroker.create()]
+    brokers: list[BrokerClient] = [BybitBroker.create(), BinanceBroker.create()]
 
     _ = await asyncio.gather(*[b.connect() for b in brokers])
 
