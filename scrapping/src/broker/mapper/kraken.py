c0ed244c1c5010e3188233769f9d94151d195daa
AarMagic <aaroof04@gmail.com>
2026-03-24 17:34:21 +0100

    feat: Agregar mapeo de datos y soporte para Kraken en el sistema de brokers

--- scrapping/src/broker/mapper/kraken.py
+++ scrapping/src/broker/mapper/kraken.py
@@ -1,12 +1,22 @@
-from broker.mapper.model.time_series_cripto import TSData
+from datetime import datetime, timezone
+
+from broker.mapper.model.time_series_cripto import Provider, TSData
 
 
 class KrakenDataMapper:
     @staticmethod
     def mapResponse(data):
+        iso_timestamp = data["data"][0]["timestamp"]
+        epoch_ms = int(
+            datetime.fromisoformat(iso_timestamp)
+            .replace(tzinfo=timezone.utc)
+            .timestamp()
+            * 1000
+        )
         return TSData(
-            timestamp=data['data'][0]['timestamp'],
-            name=data["data"][0]['symbol'],
-            price=data["data"][0]['bid'],
-            volume=data["data"][0]['volume'],
+            timestamp=epoch_ms,
+            name=data["data"][0]["symbol"],
+            price=data["data"][0]["bid"],
+            volume=data["data"][0]["volume"],
+            provider=Provider.KRAKEN,
         )
